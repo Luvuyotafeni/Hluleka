fcb8e577562f7f370b8193eee216f0a108c437a2
luvuyo <luvuyotafeni012@gmail.com>
2024-03-06 19:12:21 +0200

    galllery

--- Server/Models/Cart.js
+++ Server/Models/Cart.js
@@ -1,19 +1,19 @@
 // models/cart.js
 const mongoose = require('mongoose');
 
+const cartItemSchema = new mongoose.Schema({
+    title: String,
+    quantity: String,
+    price: String,
+});
+
 const cartSchema = new mongoose.Schema({
     name: String,
     surname: String,
     gender: String,
     id: String,
     date: String,
-    cartItems: [
-        {
-            title: String,
-            quantity: String,
-            price: String,
-        }
-    ],
+    cartItems: [cartItemSchema],
 });
 
 module.exports = mongoose.model("Cart", cartSchema);
